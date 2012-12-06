9305cffc5b6c05cc09929ea7fd96a55cc53ac7b0
sahch <saul.hormazabal@lukzel.com>
2012-12-06 15:23:12 -0300

    Redimencion de imagenes desde diferentes fuentes: <input type='file'> , <img> ,  , 'image.jpg'

--- ResizeFileImage.js
+++ ResizeFileImage.js
@@ -1,8 +1,4 @@
-function ResizeFileImage (src,width,height) {
-	
-	var img = new Image();
-	img.src = src;
-	console.log(img);
+function ResizeFileImage ( source, callback, width, height, format ) {
 
 	var buffer    = document.createElement('canvas');
 
@@ -11,16 +7,64 @@ function ResizeFileImage (src,width,height) {
 
 	var bufferCtx = buffer.getContext('2d');
 
-	console.log(buffer);
+	if ( typeof source == 'object' && source.length >= 1 ) {
+		source = source[0];
+	}
+	
+	switch (source.tagName) {
+		case 'INPUT':
+			//var file = document.getElementById(id).files[0];
+			var file = source.files[0];
+			loadFromFile(file);
+			break;
+		case 'IMG':
+			var src = source.src;
+			loadFromSrc(src);
+			break;
+		default:
 
-    img.onload = function() {
+			if ( typeof source === 'string' ) {
+				loadFromSrc(source);
+			}
 
+			break;
+	}
+
+
+    function loadFromFile (file) {
+		
+		var reader = new FileReader();
+	    reader.onload = function(e){
+
+	    	var data = e.target.result;
+			loadFromSrc(data);
+
+	    }
+	    reader.readAsDataURL(file);
+
+    }
+
+    function loadFromSrc (src) {
+
+		var img = new Image();
+
+	 	img.onload = function() {
+	 		response(this);
+	    };
+
+		img.src = src;
+
+    }
+
+    function response (img) {
+		
 		bufferCtx.drawImage(img, 0, 0, width, height);
 
-		var data = buffer.toDataURL();
+		var data = buffer.toDataURL(format);
 
-		return data;
-    };
+		callback(data);
+
+    }
 
 
 }
